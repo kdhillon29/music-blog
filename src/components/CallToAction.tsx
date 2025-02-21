f6f4bba684dddba877fbce29d0282058a5d86d76
kanwar Dhillon <kdhillon469@gmail.com>
2025-02-21 22:37:26 +1100

    fix shop index page

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -25,23 +25,23 @@ function CallToAction({data}:props){
     const product = { title: data.title , id: data.id , thumbnail: data.thumbnails[0] , price: data.price , quantity:productCount , finalPrice:finalPrice};    
 
 return(
-        <div class="flex items-center md:space-x-4 w-2/3 sm:w-full sm:flex-col sm:space-y-4" onChange={()=>calculatePrice(productCount,data.price)}>
-           <div class="flex bg-gray-200 dark:bg-gray-800 w-60 sm:w-full justify-between px-3  py-5  rounded-xl ">
-                <button class="text-yellow-500" aria-label="Decrease" onClick={()=>decreaseCounter(product.id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+        <div class=" w-full py-3 md:py-6 flex md:flex-col items-center justify-center gap-3  " onChange={()=>calculatePrice(productCount,data.price)}>
+           <div class="  flex justify-center bg-gray-200 dark:bg-gray-800 gap-2  w-18 p-2 md:w-24 md:p-2  rounded-xl ">
+                <button class="text-teal-500" aria-label="Decrease" onClick={()=>decreaseCounter(product.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                         <path  d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"/>
                     </svg>
                 </button>
-                <h1 class="text-xl sm:text-lg font-Kumbh font-bold text-gray-900 dark:text-white">{productCount}</h1>
-                <button class="text-yellow-500" aria-label="Increase" onClick={()=>increaseCounter(product.id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                <h1 class=" font-bold text-gray-900 dark:text-white">{productCount}</h1>
+                <button class="text-teal-500" aria-label="Increase" onClick={()=>increaseCounter(product.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                         <path  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"/>
                     </svg>
                 </button>
            </div>
-            <button class=" flex w-full justify-center ml-auto py-5 sm:mb-10  font-Kumbh font-semibold  rounded-xl bg-yellow-500 text-white shadow-md hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-opacity-75" 
+            <button class=" inline-flex items-center md:w-2/3 p-2 md:p-4  font-semibold  rounded-xl bg-teal-700 text-white shadow-md hover:bg-teal-300 focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-opacity-75" 
             onClick={() => {if( productCount > 0) addProduct(product,productCount)}}>
-                <svg class="mr-4" width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="currentColor" fill-rule="nonzero"/></svg>
+                <svg class="mr-1 " width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="currentColor" fill-rule="nonzero"/></svg>
                 add to cart!
             </button>
         </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,15 +8,15 @@ interface Props {
 function Card({product}: Props){
   console.log("product is: ", product);
     return(
-      <div class="w-full  m-2 flex">
-        <button class=" w-60 h-60 mb-10 md:mr-10  bg-black rounded-2xl overflow-hidden transition duration-500 ease-in-out hover:scale-110" onClick={()=> window.location.href = product.url}>
-          <img class="w-full h-full object-cover" src={product.frontmatter.img[0]} alt={product.frontmatter.title}/>
+      <div class="w-full flex max-sm:flex-col   justify-between items-center gap-2 ">
+        <button class=" w-full md:w-1/2 h-auto  bg-black rounded-2xl overflow-hidden transition duration-500 ease-in-out hover:scale-110" onClick={()=> window.location.href = product.url}>
+          <img class=" w-full md:h-64   bg-neutral-300  bg-blend-normal  object-cover" src={product.frontmatter.img[0]} alt={product.frontmatter.title}/>
         </button>
-        <div class=" w-2/3 py-3 flex flex-col gap-3  items-center ">
+        <div class=" w-full md:w-1/2 py-3 flex flex-col gap-1  items-center ">
 
-          <p class="text-xl font-semibold">{product.frontmatter.title}</p>
-          <p class="font-semibold">{product.frontmatter.price}</p>
-          <p class="text-gray-500 text-sm line-clamp-1">{product.frontmatter.description}</p>
+          <p class="text-md md:text-xl font-semibold">{product.frontmatter.title}</p>
+          <p class=" text-md md:text-base font-semibold">{product.frontmatter.price}</p>
+          <p class="text-gray-500 w-2/3 md:w-full text-center text-sm md:text-md line-clamp-2">{product.frontmatter.description}</p>
           {/* <button class="bg-violet-500/80 text-white px-2 py-1 rounded-full" onClick={()=> addProduct(product)}>Add To cart</button> */}
           <CallToAction data={ {title: product.frontmatter.title , id: product.frontmatter.id , thumbnails: product.frontmatter.thumbnails , price: product.frontmatter.price}} />
            
