e173a4abefdabf36ed93ee0d17d6b81114fe4a74
kanwar Dhillon <kdhillon469@gmail.com>
2025-03-04 20:21:48 +1100

    fix contect form layout

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -51,9 +51,9 @@ function MobileNav(){
                 </ul>
             </div>
             <div class="fixed z-30 left-0 top-0 bottom-0 w-60 bg-white dark:bg-gray-600" ref={addRef}>
-                <ul class="flex items-end pl-6 pt-6 flex-col space-y-8">
-                    <li class="mb-5">
-                        <a class="text-gray-900 dark:text-white" onClick={()=>setIsOpen(false)}>
+                <ul class="flex items-center pl-6 pt-6 flex-col space-y-8">
+                    <li class=" bg-neutral-300 w-12 ml-auto p-2 rounded-xl  mb-5">
+                        <a class="text-gray-900 dark:text-white  " onClick={()=>setIsOpen(false)}>
                             <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="currentColor" fill-rule="evenodd"/></svg>
                         </a>  
                     </li>
